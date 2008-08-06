3a3c5947b13574a618eed28747c07c67f6f82eb3
IenagaEiji <eiji.ienaga@gmail.com>
2008-08-07 06:45:09 +0900

    support  camel_case search

--- Support/lib/easyopen/find_alc.rb
+++ Support/lib/easyopen/find_alc.rb
@@ -1,4 +1,20 @@
-ENV["TM_CURRENT_WORD"].split("_").each{|word|
+class String
+  ##
+  # @return <String> The string converted to snake case.
+  #
+  # @example
+  #   "FooBar".snake_case #=> "foo_bar"
+  # @example
+  #   "HeadlineCNNNews".snake_case #=> "headline_cnn_news"
+  # @example
+  #   "CNN".snake_case #=> "cnn"
+  def snake_case
+    return self.downcase if self =~ /^[A-Z]+$/
+    self.gsub(/([A-Z]+)(?=[A-Z][a-z]?)|\B[A-Z]/, '_\&') =~ /_*(.*)/
+      return $+.downcase
+  end
+end
+
+ENV["TM_CURRENT_WORD"].snake_case.split("_").each{|word|
   `open "http://eow.alc.co.jp/#{word}/UTF-8"` 
 }
-
